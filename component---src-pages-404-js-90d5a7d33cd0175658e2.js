560fe225de80871524ce936415bf8e099bc31c9a
Chris Brooks <chris@cbrooks.dev>
2021-04-01 13:01:17 -0500

    Updates

---
+++ component---src-pages-404-js-90d5a7d33cd0175658e2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("1Qp6");t.default=function(){return l.a.createElement(o.e,null,l.a.createElement(o.f,{title:"404: Not found",location:"/404"}),l.a.createElement("h1",null,"PAGE NOT FOUND"),l.a.createElement("p",null,"Return to homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-90d5a7d33cd0175658e2.js.map
