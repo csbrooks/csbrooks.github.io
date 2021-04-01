1bad01b9b0ee2e1df1a08fbafa403d9afcc736ad
Chris Brooks <chris@cbrooks.dev>
2021-04-01 11:06:13 -0500

    First edits

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,7 +4,7 @@ import { Layout, SEO } from 'components/common';
 export default () => (
   <Layout>
     <SEO title="404: Not found" location="/404" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>PAGE NOT FOUND</h1>
+    <p>Return to homepage</p>
   </Layout>
 );
